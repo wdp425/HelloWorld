b650209f61e991a512f5e344afe8fbe22b7aa34f
david <wdp425@163.com>
2018-08-29 17:06:49 +0800

    Initial commit

---
+++ HelloWorld.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# coding:utf-8
+
+"""
+@author: David
+@software: PyCharm
+@file: HelloWorld.py
+@time: 2018/8/28 23:52
+"""
+
+print("hello world!")
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
